48f7ab62699c88efeca751428d12c225a157dfb1
Guillem <guillem@disedit.com>
2026-03-20 10:12:14 +0100

    add meta tags and title

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,21 @@
+<script setup>
+/* SEO Metatags */
+const siteName = 'Greens and Progressives in the European Committee of the Regions'
+const title = 'Greens and Progressives in the European Committee of the Regions'
+const ogTitle = title
+const description = 'We are currently working on a new website. In the meantime, you can find us on our social media channels.'
+const keywords = 'greens, progressives, committe of the regions, europe, political group, eu, cor'
+useServerSeoMeta({
+  title,
+  ogTitle,
+  description,
+  ogDescription: description,
+  keywords,
+  ogSiteName: siteName,
+})
+useHead({ title })
+</script>
+
 <template>
   <div class="bg-green-soft p-base sm:p-10 2xl:p-inner h-svh flex flex-col gap-4">
     <div class="bg-white h-full p-base sm:p-10 2xl:p-inner flex flex-col rounded-tr-[4rem] rounded-bl-[4rem] md:rounded-tr-[7rem] md:rounded-bl-[7rem] 2xl:rounded-tr-[10rem] 2xl:rounded-bl-[10rem]">
